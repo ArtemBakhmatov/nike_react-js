4a83e49b30cfb21802ae4988b7c9448ba908b93e
artem_bakhmatov <ip.bakhmaov@mail.ru>
2023-09-05 21:17:43 +0300

    Customer Reviews Section

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,7 +8,9 @@ import {
     bigShoe3, 
     thumbnailShoe1, 
     thumbnailShoe2, 
-    thumbnailShoe3 
+    thumbnailShoe3,
+    customer1,
+    customer2 
 } from "../assets/images";
 
 import { shieldTick, support, truckFast } from "../assets/icons";
@@ -80,4 +82,19 @@ export const services = [
         label: "Love to help you",
         subtext: "Our dedicated team is here to assist you every step of the way."
     },
+];
+
+export const reviews = [
+    {
+        imgURL: customer1,
+        customerName: 'Morich Brown',
+        rating: 4.5,
+        feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
+    },
+    {
+        imgURL: customer2,
+        customerName: 'Lota Mongeskar',
+        rating: 4.5,
+        feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
+    }
 ];
