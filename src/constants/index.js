b74da02c696ec406490b8f2714be4e7814737f99
artem_bakhmatov <ip.bakhmaov@mail.ru>
2023-09-06 09:45:37 +0300

    Footer

--- src/constants/index.js
+++ src/constants/index.js
@@ -13,7 +13,7 @@ import {
     customer2 
 } from "../assets/images";
 
-import { shieldTick, support, truckFast } from "../assets/icons";
+import { shieldTick, support, truckFast, facebook, twitter, instagram } from "../assets/icons";
 
 export const navLinks = [
     { href: "#home", label: "Home" },
@@ -97,4 +97,41 @@ export const reviews = [
         rating: 4.5,
         feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
     }
+];
+
+export const footerLinks = [
+    {
+        title: "Products",
+        links: [
+            { name: "Air Force 1", link: "/" },
+            { name: "Air Max 1", link: "/" },
+            { name: "Air Jordan 1", link: "/" },
+            { name: "Air Force 2", link: "/" },
+            { name: "Nike Waffle Racer", link: "/" },
+            { name: "Nike Cortez", link: "/" },
+        ],
+    },
+    {
+        title: "Help",
+        links: [
+            { name: "About us", link: "/" },
+            { name: "FAQs", link: "/" },
+            { name: "How it works", link: "/" },
+            { name: "Privacy policy", link: "/" },
+            { name: "Payment policy", link: "/" },
+        ],
+    },
+    {
+        title: "Get in touch",
+        links: [
+            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+            { name: "+92554862354", link: "tel:+92554862354" },
+        ],
+    },
+];
+
+export const socialMedia = [
+    { src: facebook, alt: "facebook logo" },
+    { src: twitter, alt: "twitter logo" },
+    { src: instagram, alt: "instagram logo" },
 ];
