4306a3c991526850f4e79c88baed9bce03f95562
artem_bakhmatov <ip.bakhmaov@mail.ru>
2023-09-08 11:27:19 +0300

    Nav при нажатии на гамбургер

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,6 +7,7 @@ import ButtonThemeColor from './ButtonThemeColor';
 
 const Nav = () => {
     const [theme, setTheme] = useState(null);
+    const [navMenu, setNavMenu] = useState('nav-menu');
 
     useEffect(() =>{
         if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -27,6 +28,32 @@ const Nav = () => {
     const handleThemeColor = () => {
         setTheme(theme === 'dark' ? 'light' : 'dark');
     }
+
+    useEffect(() => {
+        if (navMenu === 'nav-menu-active') {
+            document.querySelector('.nav-menu').classList.add('nav-menu-active');
+            document.querySelector('.nav-menu').classList.add('fade');
+            document.querySelector('.close').style.display = 'block';
+            document.querySelector('.hamburger').style.display = 'none';
+            document.querySelector('.main__title').style.zIndex = '0'
+            document.body.style.overflowY = 'hidden'
+        } else {
+            document.querySelector('.nav-menu').classList.remove('nav-menu-active');
+            document.querySelector('.nav-menu').classList.remove('fade');
+            document.querySelector('.close').style.display = '';
+            document.querySelector('.hamburger').style.display = '';
+            document.querySelector('.main__title').style.zIndex = ''
+            document.body.style.overflowY = ''
+        }
+    },[navMenu])
+
+    const handleClickMenu = () => {
+        setNavMenu('nav-menu-active');
+    }
+
+    const closeClickMenu = () => {
+        setNavMenu('nav-menu');
+    }
     return (
         <header className='padding-x py-8 absolute z-10 w-full'>
             <nav className='flex justify-between items-center max-container'>
@@ -38,12 +65,13 @@ const Nav = () => {
                         height={ 29 }
                     />
                 </a>
-                <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                <ul className='flex-1 flex justify-center items-center gap-16 nav-menu'>
                     { navLinks.map(item => 
                         <li key={ item.label }>
                             <a 
+                                onClick={ () => closeClickMenu()}
                                 href={ item.href }
-                                className='font-montserrat leading-normal text-lg text-slate-gray dark:text-slate-100'
+                                className='font-montserrat leading-normal text-lg text-slate-gray dark:text-slate-100 max-lg:text-xl max-lg:text-gray-200 max-lg:font-bold'
                             >{ item.label }</a>
                         </li>
                     )}
@@ -54,13 +82,17 @@ const Nav = () => {
                         />
                     </li>
                 </ul>
-                <div className='hidden max-lg:block'>
+                <div className='hidden max-lg:block hamburger'>
                     <img 
                         src={ theme === 'dark' ? hamburgerWhite : hamburgerBlack } 
                         alt="Hamburger" 
                         width={ 25 }
                         height={ 25 }
+                        onClick={ () => handleClickMenu() }
                     />
+                </div>
+                <div className='hidden close' onClick={ () => closeClickMenu()}>
+                    &times;
                 </div>
             </nav>
         </header>
